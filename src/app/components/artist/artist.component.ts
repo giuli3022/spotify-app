bbe6a5c4b5417bfbeeb00ef79686c9f17d3fb46c
Giuliana <giuliana.cecchetto@syloper.com>
2021-06-16 10:06:03 -0300

    artist

--- src/app/components/artist/artist.component.ts
+++ src/app/components/artist/artist.component.ts
@@ -9,17 +9,34 @@ import { SpotifyService } from 'src/app/services/spotify.service';
 })
 export class ArtistComponent {
   artist: any = {};
+  topTracks: any[] = [];
+
+  loadingArtist: boolean;
 
   constructor(private router: ActivatedRoute, private spotify: SpotifyService) {
+    this.loadingArtist = true;
+
     this.router.params.subscribe((params) => {
       this.getArtist(params['id']);
+      this.getTopTracks(params['id']);
     });
   }
 
   getArtist(id: string) {
+    this.loadingArtist = true;
+
     this.spotify.getArtist(id).subscribe((artistData) => {
       console.log(artistData);
       this.artist = artistData;
+      this.loadingArtist = false;
+    });
+  }
+
+  getTopTracks(id: string) {
+//    this.loadingArtist = true;
+    this.spotify.getTopTracks(id).subscribe((topTracks) => {
+      console.log(topTracks);
+      this.topTracks = topTracks;
     });
   }
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -3,38 +3,42 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SpotifyService {
-
-  constructor(private http: HttpClient) {
-
-  }
+  constructor(private http: HttpClient) {}
 
   getQuery(query: string) {
     const url = `https://api.spotify.com/v1/${query}`;
 
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQDmAswxqsRLpTt-h--FrxOhGr7qbgWnp00pV-aePE8yB3nkcvxnrrFMlMFmSli1SS43S8qvRvDX9RjtDZE'
+      Authorization:
+        'Bearer BQAsaiKpHNapAMc7WiJdTrameNjlPrsX5LUlNso_-y6oHnve-vbSQ5jae7H_7Eo88O70BsX8DlnDyhi390A',
     });
 
-    return this.http.get(url, { headers })
+    return this.http.get(url, { headers });
   }
 
   getNewRealeases() {
-    return this.getQuery('browse/new-releases?limit=20')
-      .pipe(map(data => data['albums'].items));
+    return this.getQuery('browse/new-releases?limit=20').pipe(
+      map((data) => data['albums'].items)
+    );
   }
 
   getResults(termino: string) {
-    return this.getQuery(`search?q=${termino}&type=artist&limit=10`)
-    .pipe(map(data => data['artists'].items));
+    return this.getQuery(`search?q=${termino}&type=artist&limit=10`).pipe(
+      map((data) => data['artists'].items)
+    );
   }
 
   getArtist(id: string) {
-
-    return this.getQuery(`artists/${ id }`);
+    return this.getQuery(`artists/${id}`);
     //.pipe(map(data => data['artists'].items));
   }
 
+  getTopTracks(id: string) {
+    return this.getQuery(`artists/${id}/top-tracks?country=us`).pipe(
+      map((data) => data['tracks'])
+    );
+  }
 }
